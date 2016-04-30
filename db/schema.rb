d0583be5e5c9119fbcc9d352f607d414054b1a8e
rmad17 <souravbasu17@gmail.com>
2016-05-01 01:26:38 +0530

    Migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430110223) do
+ActiveRecord::Schema.define(version: 20160430191133) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
