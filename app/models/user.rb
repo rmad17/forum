128589cc8ec47c8f9cdbf49edef7f3010e2e1d94
rmad17 <souravbasu17@gmail.com>
2016-05-01 13:54:22 +0530

    Added password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -35,6 +36,16 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
