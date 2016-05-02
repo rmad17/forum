19a5b79eb5c6d023055a761bc3f4aaa8acc7216a
rmad17 <souravbasu17@gmail.com>
2016-05-02 12:57:11 +0530

    Added login controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  root   'static_pages#home'
-  get   'static_pages/help'
-  get   'static_pages/about'
-  get   'static_pages/about'
-  get   'signup'  => 'users#new'
+  # get 'sessions/new'
+
+  root      'static_pages#home'
+  get       'static_pages/help'
+  get       'static_pages/about'
+  get       'static_pages/about'
+  get       'signup'  => 'users#new'
+  get       'login'   => 'sessions#new'
+  post      'login'   => 'sessions#create'
+  delete    'logout'  => 'sessions#destroy'
 
   resources :users
 
