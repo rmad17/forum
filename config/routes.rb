060fe6081dbd0b15bb93fa37ff04e76f8ff5e6a5
rmad17 <souravbasu17@gmail.com>
2016-05-03 13:22:00 +0530

    Installed forem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # This line mounts Forem's routes at /forums by default.
+  # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
+  # If you would like to change where this extension is mounted, simply change the :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
+  mount Forem::Engine, :at => '/forums'
+
   # get 'sessions/new'
 
   root      'sessions#new'
@@ -7,6 +15,7 @@ Rails.application.routes.draw do
   get       'static_pages/about'
   get       'static_pages/about'
   get       'signup'  => 'users#new'
+  get       '/users/sign_in', :to => "sessions#new"
   get       'login'   => 'sessions#new'
   post      'login'   => 'sessions#create'
   delete    'logout'  => 'sessions#destroy'
