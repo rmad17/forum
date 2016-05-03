ab6494cc366c139ab2fe2e46be6d3806e5bee9cb
rmad17 <souravbasu17@gmail.com>
2016-05-03 10:16:03 +0530

    made login page as route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # get 'sessions/new'
 
-  root      'static_pages#home'
+  root      'sessions#new'
+  get       'static_pages/home'
   get       'static_pages/help'
   get       'static_pages/about'
   get       'static_pages/about'
